90ffae8df8f67063eaf9a5aba225c23a39941cb8
Mikk3lRo <mikk3lro@gmail.com>
2018-05-02 21:42:46 +0200

    Turns out usleep is not needed :)

--- tests/ProcessesTest.php
+++ tests/ProcessesTest.php
@@ -8,17 +8,15 @@ use Mikk3lRo\atomix\utilities\Processes;
 final class ProcessesTest extends TestCase
 {
     public function testExecuteNonBlockingRunsAndDoesNotBlock() {
-        echo `usleep 10000000 2>&1`;
-
         $starttime = microtime(true);
-        $pid = Processes::executeNonBlocking('date;echo abc;usleep 100000;echo def;date;');
+        $pid = Processes::executeNonBlocking('date;echo abc;sleep 0.1;echo def;date;');
         $this->assertGreaterThan(0, $pid);
         $executiontime = microtime(true) - $starttime;
         $this->assertLessThan(.05, $executiontime);
     }
     public function testCanDetermineIfProcessIsRunning() {
         $starttime = microtime(true);
-        $pid = Processes::executeNonBlocking('date;echo abc;usleep 100000;echo def;date;');
+        $pid = Processes::executeNonBlocking('date;echo abc;sleep 0.1;echo def;date;');
         $this->assertGreaterThan(0, $pid);
         $this->assertEquals(true, Processes::isRunning($pid));
         usleep(200000);
@@ -26,7 +24,7 @@ final class ProcessesTest extends TestCase
     }
     public function testExecuteNonBlockingSimpleCommand() {
         $starttime = microtime(true);
-        $pid = Processes::executeNonBlocking('usleep 10000000');
+        $pid = Processes::executeNonBlocking('usleep 0.1');
         $this->assertGreaterThan(0, $pid);
         $this->assertEquals(true, Processes::isRunning($pid));
         $executiontime = microtime(true) - $starttime;
@@ -34,7 +32,7 @@ final class ProcessesTest extends TestCase
     }
 
     public function testExecuteNonBlockingDoesOutput() {
-        $pid = Processes::executeNonBlocking('echo -n abc;echo -n ghi >&2;usleep 100000;echo -n def;echo -n jkl >&2', '/tmp/stdout', '/tmp/stderr');
+        $pid = Processes::executeNonBlocking('echo -n abc;echo -n ghi >&2;sleep 0.1;echo -n def;echo -n jkl >&2', '/tmp/stdout', '/tmp/stderr');
         usleep(50000);
         $this->assertRegExp('#^abc$#', file_get_contents('/tmp/stdout'));
         $this->assertRegExp('#^ghi$#', file_get_contents('/tmp/stderr'));
